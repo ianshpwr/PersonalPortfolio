f057b63c8230d8c3f681a2eaba3f3a5a6b58968f
ansh <ianshpwr@gmail.com>
2025-06-17 13:49:03 +0530

    added tracing beams

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Navbar from './../components/navbar/navbar';
-
+import TracingBeamm from '@/components/tracing';
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <TracingBeamm/>
     </div>
   );
 }
