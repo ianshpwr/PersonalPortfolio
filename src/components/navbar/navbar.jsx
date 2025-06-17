4ae010d95f9ef669ff6c4b5ef000ce5e5d5ddf88
ansh <ianshpwr@gmail.com>
2025-06-17 14:22:26 +0530

    added logo of ANSHX

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,9 +5,9 @@ import './navbar.css';
 export default function Navbar(){
     return(
     <div className="navbar-container">
-        <div className='logo'>
-            <h3>Logo</h3>
-        </div>
+<div className='logo'>
+    <img src='/logo.png' alt='Logo' />
+</div>
         <div className="nav-links">
         <Link href="/about">About</Link>
         <Link href="/skills">Skills</Link>
